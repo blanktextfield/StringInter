c97a02014574b79d0c57423238d1c9f1649274e1
Opilane TTHK <opilane@tthk.ee>
2020-09-10 12:28:05 +0300

    StringInter

---
+++ StringInter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StringInter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 1. The app asks for the user`s first name and stores it in a seperate variable
+             * 2. The app asks for the user`s last name and stores it in a seperate variable
+              */
+            string FirstName;
+            string LastName;
+            Console.WriteLine("What`s your first name?");
+            FirstName = Console.ReadLine();
+            Console.WriteLine("what`s your last name?");
+            LastName = Console.ReadLine();
+            Console.WriteLine("Hello," + FirstName + " " + LastName);
+            Console.WriteLine($"Hello, ");
+            //Console.Write("Hello, + Fisrtname + " " + Lastname);
+            //string interpolation
+            //$ - alt + ctrl+4 {alt gr + 7,} - alt gr + 0
+            
+
+            
+
+
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1. program asks for user`s first and lastname
+             * 2. program asks for user`s year of birth
+             * 3.program greets the user by their name and displays their age
+             */
+            string FirstName;
+            Console.WriteLine("What`s your full name?");
+            FirstName = Console.ReadLine();
+            
+          
+            Console.WriteLine("Hello," + FirstName + " ");
+
+            Console.WriteLine("What`s user`s year of birth");
+            string userinput;
+            userinput = Console.ReadLine();
+            int Yearofbirth;
+            //parsing string to interger (number)
+            Yearofbirth = Int32.Parse(userinput);
+            int Age = 2020 - Yearofbirth;
+            Console.WriteLine("Hello " + FirstName + " you`re " + Age + " years old");
+
+
+
+        }
+    }
+}
